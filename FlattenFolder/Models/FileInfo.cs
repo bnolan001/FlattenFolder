4fce2d8e7965b12d3cf49543d3d0b18e12a92183
Brian Nolan <bnolan001@gmail.com>
2024-03-11 21:59:26 -0400

    initial code

---
+++ FlattenFolder/Models/FileInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace FlattenFolder.Models
+{
+    public class FileInfo
+    {
+        public string FileName { get; set; }
+
+        public string Hash { get; set; }
+
+        public string Extension { get; set; }
+
+        public string Path { get; set; }
+    }
+}
---
+++ FlattenFolder/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("What folder to flatten");
+var folderPath = Console.ReadLine();
+
+var files = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+var filteredFiles = files.Where(f => Regex.IsMatch(f.ToLower(), "/*.(jpg|jpeg|png)$"));
+var fileDataList = new Dictionary<string, List<FlattenFolder.Models.FileInfo>>();
+foreach (var file in filteredFiles)
+{
+    Console.WriteLine(file);
+    var fileName = Path.GetFileName(file);
+    var fileStream = new FileStream(file, FileMode.OpenOrCreate,
+                FileAccess.Read);
+    var checksum = GetChecksumBuffered(fileStream);
+    var fileInfo = new FlattenFolder.Models.FileInfo
+    {
+        FileName = fileName,
+        Hash = checksum,
+        Extension = Path.GetExtension(fileName),
+        Path = file
+    };
+
+    if (fileDataList.ContainsKey(checksum))
+    {
+        fileDataList[checksum].Add(fileInfo);
+    }
+    else
+    {
+        fileDataList.Add(checksum, new List<FlattenFolder.Models.FileInfo> { fileInfo });
+    }
+}
+
+var duplicates = fileDataList.Where(f => f.Value.Count > 1);
+
+foreach (var duplicate in fileDataList)
+{
+    if (duplicate.Value.Count > 1)
+    {
+        Console.WriteLine($"Duplicate found: {String.Join(", ", duplicate.Value.Select(f => f.FileName).ToList())}");
+        foreach (var file in duplicate.Value)
+        {
+            Console.WriteLine(file);
+        }
+    }
+}
+static string GetChecksumBuffered(Stream stream)
+{
+    using (var bufferedStream = new BufferedStream(stream, 16 * 1024 * 1024))
+    {
+        var sha = SHA256.Create();
+        byte[] checksum = sha.ComputeHash(bufferedStream);
+        return BitConverter.ToString(checksum).Replace("-", String.Empty);
+    }
+}
