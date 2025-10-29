5ce467905a3a6c6dfb6ed97e53fb9a75d683104f
Brian Nolan <bnolan001@gmail.com>
2025-10-29 07:45:55 -0400

    Added file extensions to cover more formats for office, audio, video, and images

--- FlattenFolder/Statics/FileExtensions.cs
+++ FlattenFolder/Statics/FileExtensions.cs
@@ -2,12 +2,14 @@
 {
     public class FileExtensions
     {
-        public static string[] ImageExtensions = new string[] { "jpg", "jpeg", "png", "tiff", "bmp", "tif", "gif" };
+        public static string[] ImageExtensions = new string[] { "jpg", "jpeg", "png", "tiff", "bmp", "tif", "gif", 
+            "webp", "avif", "raw", "svg", "cr2", "nef" };
 
-        public static string[] VideoExtensions = new string[] { "mp4", "mpg", "avi", "mov" };
+        public static string[] VideoExtensions = new string[] { "mp4", "mpg", "avi", "mov", "wmf", "mkv" };
 
-        public static string[] OfficeDocumentExtnsions = new string[] { "doc", "docx", "xls", "xlsx", "ppt", "pptx", "odp", "ods", "odg", "odt" };
+        public static string[] OfficeDocumentExtnsions = new string[] { "doc", "docx", "docm", "xls", "xlsx", "xlsm", 
+            "ppt", "pptx", "pptm", "dotx", "dotm", "odp", "ods", "odg", "odt", "odf" };
 
-        public static string[] AudioExtensions = new string[] { "mp3", "wav", "wma", "ogg", "flac" };
+        public static string[] AudioExtensions = new string[] { "mp3", "wav", "wma", "ogg", "flac", "aac", "m4a", "aiff" };
     }
 }
